2be4eef85d2e5e7ea151a0236e71d7ff961cd4c8
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-12-17 12:55:47 +0300

    create the form, added ngModel directive to make the form be detected as an angular. used bootstrap@5 to style the form. accesse the form via viewchild to view the form in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '';
+
+  @ViewChild('form') signupForm: ElementRef
+  quote: string;
+  onSubmitForm(){
+    console.log(this.signupForm)
+  }
 }
