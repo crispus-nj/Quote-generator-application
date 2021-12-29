5bb02812bc9ef416cf3dd55fc0523604a1efc224
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-12-29 12:09:12 +0300

    added another property to the quote array, added a selection to the form for a user to select vote status to either upvote or downvote

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -3,24 +3,28 @@ export class FormService{
         {
           name: "crispu Njenga",
           email: "njengacs@gmail.com",
-          quote: "No pain no gain"
+          quote: "No pain no gain",
+          status: "Upvote"
         },
         {
           name: "Levis Oleguna",
           email: "oleguna@gmail.com",
-          quote: "Hardwork Pays"
+          quote: "Hardwork Pays",
+          status: "Downvote"
         },
         {
           name: "Kioko masaai",
           email: "masaai@gmail.com",
-          quote: "focus on future"
+          quote: "focus on future",
+          status: "Upvote"
         }
       ]
-      onFormData(name: string, email: string, quote: string){
+      onFormData(name: string, email: string, quote: string, status: string){
         this.quotes.push({
             name: name,
             email: email,
-            quote: quote
+            quote: quote,
+            status: status
         })
       }
 }
