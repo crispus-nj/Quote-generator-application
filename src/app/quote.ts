65f03bc910b0f011f93daa49e088b6a9478fd04f
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-05 14:24:17 +0300

    add quote file which contains quote class

---
+++ src/app/quote.ts
@@ -0,0 +1,8 @@
+export class Quote {
+    constructor(public id: number,
+        public name: string,
+        public email: string,
+        public quote: string,
+        public upvote: number,
+        public downvote: number) { }
+}
